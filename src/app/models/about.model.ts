837048f4b333a8a5e9026c318d4fe0bfbb936d67
daffriedman <dafriedman443@gmail.com>
2021-03-18 09:14:07 +0200

    hope this works

--- src/app/models/about.model.ts
+++ src/app/models/about.model.ts
@@ -4,17 +4,5 @@ export class About {
   email: string;
   siteworks: string;
   ppolicy: string;
-  // constructor(
-  //   about: string,
-  //   phone: string,
-  //   email: string,
-  //   siteworks: string,
-  //   ppolicy: string
-  // ) {
-  //   this.about = about;
-  //   this.phone = phone;
-  //   this.email = email;
-  //   this.siteworks = siteworks;
-  //   this.ppolicy = ppolicy;
-  // }
+ 
 }
